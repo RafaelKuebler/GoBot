93e8f216f9cd137d55877fb0c1b2076b01ec580a
RafaelKuebler <rafael_kuebler@yahoo.es>
2018-11-13 13:28:26 +0100

    Display user name when prompting for next move

--- src/main.py
+++ src/main.py
@@ -21,23 +21,29 @@ def start(bot, update):
 
 def new_game(bot, update):
     chat_id = update.message.chat_id
-    game_handler.new_game(chat_id, update.message.from_user.id)
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.name.replace('\'', '')
+    game_handler.new_game(chat_id, (user_id, user_name))
     send_message(bot, chat_id, settings.new_game_text)
 
 
 def join(bot, update):
     chat_id = update.message.chat_id
-    game_handler.join(chat_id, update.message.from_user.id)
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.name.replace('\'', '')
+    game_handler.join(chat_id, (user_id, user_name))
     send_message(bot, chat_id, settings.start_game_text)
     show_board(bot, update)
 
 
 def place(bot, update):
     chat_id = update.message.chat_id
-    player = update.message.from_user.id
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.name.replace('\'', '')
     coords = update.message.text.replace('/place ', '')
     try:
-        game_handler.place_stone(chat_id, player, coords)
+        # TODO: account for user name changes
+        game_handler.place_stone(chat_id, (user_id, user_name), coords)
         show_board(bot, update)
     except GoGameException as exception:
         send_message(bot, chat_id, str(exception))
@@ -53,11 +59,11 @@ def pass_turn(bot, update):
 
 def show_board(bot, update):
     chat_id = update.message.chat_id
-    cur_player = game_handler.cur_player(chat_id)
+    _, cur_player_name = game_handler.cur_player(chat_id)
     image = game_handler.create_image(chat_id)
     bot.send_photo(chat_id, photo=image)
     # TODO: extract string
-    send_message(bot, chat_id, f"It is {cur_player}'s turn")
+    send_message(bot, chat_id, f"It is {cur_player_name}'s turn")
 
 
 def display_proverb(bot, update):
