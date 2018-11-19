8e7c55557945294f25d3e681b65e1d9db561daea
RafaelKuebler <rafael_kuebler@yahoo.es>
2018-11-20 00:25:25 +0100

    Add codeclimate and refactor

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # GoSenseiBot
 
 [![Build Status](https://travis-ci.org/RafaelKuebler/GoBot.png?branch=master)](https://travis-ci.org/RafaelKuebler/GoBot)
+[![Maintainability](https://api.codeclimate.com/v1/badges/54cc8e07132596a725d3/maintainability)](https://codeclimate.com/github/RafaelKuebler/GoBot/maintainability)
 [![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
 ![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)
 ![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)
--- gobot/go/goscreenshot.py
+++ gobot/go/goscreenshot.py
@@ -23,44 +23,52 @@ class GoScreenshot:
         self.board_y = board_y
         self.border_x = .125 * self.background_x
         self.border_y = .125 * self.background_y
-        self.width_x = (self.background_x - 2 * self.border_x) / (self.board_x - 1)
-        self.width_y = (self.background_y - 2 * self.border_y) / (self.board_y - 1)
-        self.start_x = self.background_x * .5 - self.width_x * (self.board_x - 1) * .5
-        self.start_y = self.background_y * .5 - self.width_y * (self.board_y - 1) * .5
+        self.width_x = self._calculate_cell_width(self.background_x, self.border_x, self.board_x)
+        self.width_y = self._calculate_cell_width(self.background_y, self.border_y, self.board_y)
+        self.start_x = self._calculate_start(self.background_x, self.width_x, self.board_x)
+        self.start_y = self._calculate_start(self.background_y, self.width_y, self.board_y)
 
         self.stone_size_x = self.width_x * .9
         self.stone_size_y = self.width_y * .9
+
+    @staticmethod
+    def _calculate_cell_width(background, border, board):
+        return (background - 2 * border) / (board - 1)
+
+    @staticmethod
+    def _calculate_start(background, width, board):
+        return background * .5 - width * (board - 1) * .5
 
     # TODO: create nice board graphic where indexing and board occupy space evenly
     def take_screenshot(self, board):
         for column in board.stones:
             for stone in column:
-                self.draw_stone(stone, stone == board.last_stone_placed)
+                self._draw_stone(stone, stone == board.last_stone_placed)
         bio = BytesIO()
         self.img.save(bio, settings.image_extension)
         bio.seek(0)
         return bio
 
-    def draw_stone(self, stone, marked):
+    def _draw_stone(self, stone, marked):
         if stone is None:
             return
         x, y = stone.coords
         color = self.WHITE if stone.color is Color.WHITE else self.BLACK
-        bb_start = self.calc_upper_left_corner(x, y, self.stone_size_x, self.stone_size_y)
-        bb_end = self.calc_lower_right_corner(bb_start, self.stone_size_x, self.stone_size_y)
+        bb_start = self._calc_upper_left_corner(x, y, self.stone_size_x, self.stone_size_y)
+        bb_end = self._calc_lower_right_corner(bb_start, self.stone_size_x, self.stone_size_y)
         self.draw.ellipse((bb_start, bb_end), fill=color)
 
         if marked:
             color = self.BLACK if stone.color is Color.WHITE else self.WHITE
-            bb_start = self.calc_upper_left_corner(x, y, self.stone_size_x*.5, self.stone_size_y*.5)
-            bb_end = self.calc_lower_right_corner(bb_start, self.stone_size_x*.5, self.stone_size_y*.5)
+            bb_start = self._calc_upper_left_corner(x, y, self.stone_size_x * .5, self.stone_size_y * .5)
+            bb_end = self._calc_lower_right_corner(bb_start, self.stone_size_x * .5, self.stone_size_y * .5)
             self.draw.rectangle((bb_start, bb_end), outline=color, width=3)
 
-    def calc_upper_left_corner(self, x, y, element_width_x, element_width_y):
+    def _calc_upper_left_corner(self, x, y, element_width_x, element_width_y):
         bb_start_x = self.start_x + self.width_x * x - element_width_x*.5
         bb_start_y = self.start_y + self.width_y * y - element_width_y*.5
         return bb_start_x, bb_start_y
 
-    def calc_lower_right_corner(self, upper_left_corner, element_width_x, element_width_y):
+    def _calc_lower_right_corner(self, upper_left_corner, element_width_x, element_width_y):
         bb_start_x, bb_start_y = upper_left_corner
         return bb_start_x + element_width_x, bb_start_y + element_width_y
